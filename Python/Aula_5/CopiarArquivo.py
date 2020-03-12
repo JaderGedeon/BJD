2ab0831d169f9d348794870f0a8be438eecb194a
Jader <60219669+JaderGedeon@users.noreply.github.com>
2020-03-12 11:27:53 -0300

    Aula 5 Adicionada

---
+++ Python/Aula_5/CopiarArquivo.py
@@ -0,0 +1,27 @@
+######################################
+# Disciplina: PI 2: Plataforma
+# Título: Copiar Arquivo
+# Autor: Jader G. O. Rocha
+# Data: 12/03/20
+######################################
+
+arquivoEscrito = "CopiarArquivos_Arquivos/Escrito.txt"
+arquivoBranco = "CopiarArquivos_Arquivos/Vazio.txt"
+
+
+def copiarArquivo(arquivoE, arquivoB):
+    fE = open(arquivoE, 'r')
+    fB = open(arquivoB, 'w')
+
+    while True:
+        texto = fE.readline()
+        if texto == "":
+            break
+        if texto[0] != '#':
+            fB.write(texto)
+
+    fE.close()
+    fB.close()
+
+
+copiarArquivo(arquivoEscrito, arquivoBranco)
