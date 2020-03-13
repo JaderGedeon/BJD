d8f56032cbb5683c6fa738e88de8172c8b7c076b
Jader <60219669+JaderGedeon@users.noreply.github.com>
2020-03-13 10:17:06 -0300

    Textinho inicial já alterado

--- Algoritmos II/ProjetoSudoku/src/Main.java
+++ Algoritmos II/ProjetoSudoku/src/Main.java
@@ -1,15 +1,15 @@
 /*
 Entrega de trabalho
-Nós,
+Eu,
 
 Jader Gedeon de Oliveira Rocha
 
-declaramos que
+declaro que
 
-todas as respostas são fruto de nosso próprio trabalho,
-não copiamos respostas de colegas externos à equipe,
-não disponibilizamos nossas respostas para colegas externos à equipe e
-não realizamos quaisquer outras atividades desonestas para nos beneficiar ou prejudicar
+todas as respostas são fruto de meu próprio trabalho,
+não copiei respostas de colegas externos,
+não disponibilizarei minhas respostas para colegas externos e
+não realizarei quaisquer outras atividades desonestas para me beneficiar ou prejudicar
 outros.
 */
 
